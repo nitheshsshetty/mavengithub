4f4c402b9d314b2f2a4b6c8fc0de5c6aa2cb18b5
Nithesh shetty <nsnitheshshetty71@gmail.com>
2020-08-01 22:32:28 +0530

    test git hook v.02

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -6,7 +6,7 @@ package com.mycompany.app;
 public class App
 {
 
-    private final String message = "Hello World from Nithesh Shetty!";
+    private final String message = "Hello World from Nithesh S Shetty!";
 
     public App() {}
 
